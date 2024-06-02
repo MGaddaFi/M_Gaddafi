84e5c39004085df5508c128b23e46b3293e17c28
MGaddaFi <mgaddafi455@gmail.com>
2024-06-02 23:30:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+console.log("hi");
+let m = 100;
+let n = 50;
+if (100 > 50) {
+   log("hi");
+ } else {
+   log("bye");
+ }
+
+let m = document.querySelector("button");
+function countdown() {
+  div;
+  if ((div = 10)) {
+    alert(no);
+  }
+  
+}
+const emoji = [
+  { emoji: "", name: "give us rating" },
+  { emoji: "sad", name: "very poor" },
+];
 
